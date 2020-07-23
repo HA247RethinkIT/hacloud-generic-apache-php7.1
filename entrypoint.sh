8306346b18b20ce64ccc0a8116b2f13949636248
Nick Bascombe-Fox <nick.fox@ha247.co.uk>
2020-07-23 12:33:13 +0100

    Hide php version

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ sed -i "s/PORT/$PORT/g" /run_test.sh
 
 # Set any variables we have in the env for php
 printenv |grep 'PHP_'|sed 's/^PHP_//' > /etc/php5/conf.d/user.ini
+# Hide PHP Version
+echo 'expose_php = Off' >> /etc/php5/conf.d/user.ini
 
 # Start apache
 /usr/sbin/apache2ctl -DFOREGROUND
