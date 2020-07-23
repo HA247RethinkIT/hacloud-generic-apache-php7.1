d45229fe783673bdf1c6abd8188ef13953841feb
Nick Bascombe-Fox <nick.fox@ha247.co.uk>
2020-07-23 11:13:31 +0100

    Add ports.conf & change placeholder for ports config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-sed -i "s/80/$PORT/g" /etc/apache2/ports.conf
-sed -i "s/80/$PORT/g" /etc/apache2/sites-enabled/000-default
+sed -i "s/PORT/$PORT/g" /etc/apache2/ports.conf
+sed -i "s/PORT/$PORT/g" /etc/apache2/sites-enabled/000-default
 
 
 /usr/sbin/apache2ctl -DFOREGROUND
