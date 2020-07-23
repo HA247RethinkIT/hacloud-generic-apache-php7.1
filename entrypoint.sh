c6e276cc90f346a359551d16131b48d20f0fef79
Nick Bascombe-Fox <nick.fox@ha247.co.uk>
2020-07-23 14:25:27 +0100

    Port should not change for the test

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,6 @@
 # Update Apache port
 sed -i "s/PORT/$PORT/g" /etc/apache2/ports.conf
 sed -i "s/PORT/$PORT/g" /etc/apache2/sites-enabled/000-default
-sed -i "s/PORT/$PORT/g" /run_test.sh
 
 # Set any variables we have in the env for php
 printenv |grep 'PHP_'|sed 's/^PHP_//' > /etc/php5/conf.d/user.ini
