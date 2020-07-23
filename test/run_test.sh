466a67f1dcf564e890dffa5d1cdaa39929d7f278
Nick Bascombe-Fox <nick.fox@ha247.co.uk>
2020-07-23 13:27:18 +0100

    read port as a variable

--- test/run_test.sh
+++ test/run_test.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-# Wait for haproxy to start
+# Wait for Apache to start
 sleep 2
 
 # Check we are alive
-curl --silent --fail http://localhost:PORT/index.php || exit 1
+curl --silent --fail http://localhost:${PORT}/index.php || exit 1
 
